620e4ffa4b6edb1c19c659220260b88ca4e35e40
Nathan Sharma <nathansharma007@gmail.com>
2025-02-10 00:40:57 -0600

    changes to instructions, changed apple touch icon to the favicon

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App flex flex-col items-center h-screen pt-8 justify-center bg-gray-200">
-      <h1 className="text-blue-500 text-3xl text-center font-bold font-mono py-4">Katy ISD Grade Calculator</h1>
+      <h1 className="text-blue-500 text-3xl text-center font-bold font-sans py-4">Katy ISD Grade Calculator</h1>
       <p className="font-extrabold mt-[-10px]">By Nathan Sharma</p>
 
       <div className="mt-3 border border-gray-300 p-8 rounded shadow-md">
@@ -62,8 +62,8 @@ function App() {
         <div className="mt-3 mb-4 border border-gray-300 p-8 rounded shadow-md w-1/2 mx-auto max-h-[70vh] overflow-y-auto relative">
           <h2 className = "font-bold underline">How to use the calculators</h2>
           <p className ="mt-3">Semester Calculator: Input your six weeks averages for any class into the SW fields. </p>
-          <p className = "mt-3">GPA Calculator: Entries should be in the form of Letter(Number of semesters) separated by commas. In other words, you're inputting the number of semesters where your average was the letter outside the parentheses. If you didn't take any 5.0 or 4.0 courses, put 'none' in the corresponding placeholder. Make sure you put parentheses around number of semesters.</p>
-          <p className = "mt-3">Class Average Calculator: Input your grades separated by commas or their averages into each category. </p>
+          <p className = "mt-3">GPA Calculator: Entries should be in the form of Letter(Number of semesters) separated by commas. In other words, you're inputting the number of semesters where your average was the letter outside the parentheses. If you didn't take any 5.0 or 4.0 courses, put 'none' in the corresponding placeholder. Make sure you also account for the high school credit courses you took in middle school!</p>
+          <p className = "mt-3">Class Average Calculator: Input your grades separated by commas or their averages into each category. You can also use this as a what if calculator to see what you need to get the average you're aiming for. </p>
         </div>
       )}
     </div>
